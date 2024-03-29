5c01561907c954ea251b0a489369747f20c042fd
tomieq <tomieq@wp.pl>
2024-03-29 17:03:22 +0100

    Created basic web server with bootstrap

--- Sources/Frontend.swift
+++ Sources/Frontend.swift
@@ -7,9 +7,44 @@
 
 import Foundation
 import Swifter
+import Template
 
 class Frontend {
+    
     init(_ server: HttpServer) {
+        var mainTemplate: Template {
+            Template(from: "templates/index.html")
+        }
+        server["/"] = { _, _ in
+            let template = mainTemplate
+            template.assign(["content": "Welcome to AI label system"])
+            return .ok(.html(template.output))
+        }
         
+        server.notFoundHandler = { request, responseHeaders in
+            request.disableKeepAlive = true
+            let filePath = Resource().absolutePath(for: request.path)
+            if FileManager.default.fileExists(atPath: filePath) {
+                guard let file = try? filePath.openForReading() else {
+                    print("Could not open `\(filePath)`")
+                    return .notFound()
+                }
+                let mimeType = filePath.mimeType()
+                responseHeaders.addHeader("Content-Type", mimeType)
+
+                if let attr = try? FileManager.default.attributesOfItem(atPath: filePath),
+                   let fileSize = attr[FileAttributeKey.size] as? UInt64 {
+                    responseHeaders.addHeader("Content-Length", String(fileSize))
+                }
+
+                return .raw(200, "OK", { writer in
+                    try writer.write(file)
+                    file.close()
+                })
+            }
+            print("File `\(filePath)` doesn't exist")
+            return .notFound()
+        }
     }
+    
 }
